041735c4314bd1d21f5c8b7ea7a916b03f419b1a
Sean Kidwell <dev.seankidwell@gmail.com>
2018-05-16 02:19:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,30 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var obj = {
+    itemName: item,
+    itemPrice: Math.floor(Math.random()*100)
+  }
+  cart.push(obj);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  var end = 'In your cat, you have ';
+  for (var i = 0; i < cart.length; i++) {
+    if (cart.length === 1) {
+      end = end.concat(`${cart[i].itemName} at ${cart[i].itemPrice}.`);
+    } else if (cart.length >= 2) {
+      if (i < cart.length-1) {
+        end = end.concat(`${cart[i].itemName} at ${cart[i].itemPrice}, `);
+      } else if (i === cart.length-1) {
+        end = end.concat(`and ${cart[i].itemName} at ${cart[i].itemPrice}.`)
+      }
+    } else if (cart.length === 0) {
+      end = 'Your shopping cart is empty.';
+    }
+  }
+  return end;
 }
 
 function total() {
