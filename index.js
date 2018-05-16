5f6ea0f4f2712c531cdca5b44c7d2d2b193c49cd
Sean Kidwell <dev.seankidwell@gmail.com>
2018-05-16 02:22:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (item === cart[i].itemName) {
-      cart.splice(cart[i], 1);
+      cart.splice(i, 1);
       return cart;
     } 
   }
